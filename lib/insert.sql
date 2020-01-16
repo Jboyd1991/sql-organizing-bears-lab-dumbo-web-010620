bf0768b3f52fde7b320782a4fed4d77937bcad12
Julianna Boyd <julianna.boyd91@gmail.com>
2020-01-16 16:10:09 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "brown", "good", "alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "female", "brown", "good", "alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "female", "black", "bad", "dead");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, "male", "brown", "bad", "alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "female", "white", "good", "dead");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "male", "black", "good", "dead");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "female", "brown", "bad", "alive");
+INSERT INTO bears DEFAULT VALUES;
